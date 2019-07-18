a73dd032edbe303aee9c89da1142b4d0dd00b79d
x3matt <x3matt21@mail.ru>
2019-07-18 23:36:55 +0700

    Added solution

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,22 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
 
+	public static void main(String[] args) {
+		List<String> dateList = Arrays.asList("2019-02-25","2011-08-30","2014-07-13");
+		System.out.println(reformator(dateList));
+	}
+	public static List<String> reformator(List<String> dateList){
+		return dateList
+				.stream()
+				.map(a -> LocalDate.parse(a))
+				.map(a -> a.format
+
+(DateTimeFormatter.ofPattern("dd MMMM yyyy EEEE")))
+				.collect(Collectors.toList());
+	}
 }
